5acad5e6fb084c2eb916fb138298fb8fd0fb5b43
Yunior Alayo <alayo.rondon.yunior.ext@ausy-technologies.de>
2023-08-02 08:04:34 +0200

    Update to work with clojurescript.

--- src/main/com/rondonsarnik/app/core.cljs
+++ src/main/com/rondonsarnik/app/core.cljs
@@ -1,6 +1,8 @@
-(ns com.rondonsarnik.app.core)
+(ns com.rondonsarnik.app.core
+  (:require ["react-dom/client" :as rdom]
+            [reagent.core  :as r]
+            [com.rondonsarnik.app.views :as views]))
 
 (defn ^:export init []
-  (js/console.log "test"))
-
-(js/alert "hello")
+  (let [root (rdom/createRoot (js/document.getElementById "app"))]
+    (.render root (r/as-element [views/app]))))
